b46d488f39a13363c5eaa5d2ed8e2b181a93388f
AndryusEsteves <andryusforever@hotmail.com>
2025-10-21 21:32:06 -0300

    Exercicio sql1

---
+++ lista_de_exercicios/exercicio_sql.sql
@@ -0,0 +1,55 @@
+-- Lista de Exercicios
+-- Mostre todos os registros da tabela Aluno. (Use SELECT e FROM)
+
+SELECT * FROM Aluno;
+
+-- Exiba apenas o nome e a nota1 de todos os alunos. 
+-- (Use SELECT com colunas específicas)
+
+SELECT nome, nota1
+FROM Aluno
+
+-- Liste todos os alunos cuja nota2 seja maior que 8. (Use WHERE)
+
+SELECT nota2 FROM Aluno
+WHERE nota2 > 8
+
+-- Mostre os alunos que nasceram após o ano de 2000. (Use WHERE com data)
+
+SELECT data_nascimento FROM Aluno
+WHERE data_nascimento > "2000-01-01"
+
+-- Exiba o nome e a mensalidade de todos os cursos que custam mais de 600 reais. 
+-- (Use WHERE com condição numérica)
+
+SELECT nome, mensalidade FROM Curso
+WHERE mensalidade > 600
+
+-- Mostre o nome das turmas e o ano correspondente, 
+-- ordenados pelo ano em ordem crescente. (Use ORDER BY)
+
+SELECT nome, ano FROM Turma
+ORDER BY ano
+
+-- Liste o ano das turmas e a quantidade de turmas por ano. (Use GROUP BY)
+
+SELECT ano, COUNT(*) AS quantidade_turma
+FROM Turma
+GROUP BY ano;
+
+-- Calcule a média da nota1 dos alunos por turma_id. 
+-- (Use GROUP BY com função de agregação)
+
+SELECT id_turma, AVG(nota1) AS media_nota1
+FROM Aluno
+GROUP BY id_turma
+
+-- Mostre o ano e a quantidade de turmas apenas para os anos que têm mais de 2 turmas. 
+--(Use GROUP BY e HAVING)
+
+
+SELECT ano, COUNT(*) AS quantidade_turma
+FROM Turma
+GROUP BY ano
+HAVING COUNT(*) > 2
+
